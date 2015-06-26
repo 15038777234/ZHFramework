71bba7618547bb9e02311d4428a3cee9bfc47ebe
hangzhang <15038777234@163.com>
2015-06-26 13:19:59 +0800

    0.0.1
    0.0.1

---
+++ Framework/ZHFramework.framework/Versions/A/Headers/ZHFramework.h
@@ -0,0 +1,13 @@
+//
+//  ZHFramework.h
+//  ZHFramework
+//
+//  Created by hangzhang on 15/6/26.
+//  Copyright (c) 2015年 hangzhang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZHFramework : NSObject
++(NSString *)sdkVersion;
+@end
